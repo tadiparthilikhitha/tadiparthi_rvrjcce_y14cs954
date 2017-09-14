04450f35395f4d3b7509b0dbc1c69105814d1cc3
tadiparthilikhitha <tadiparthilikhitha@gmail.com>
2017-09-14 22:25:04 +0530

    my 27 commit

--- src/StudentGroup.java
+++ src/StudentGroup.java
@@ -54,11 +54,11 @@ public class StudentGroup implements StudentArrayOperation {
 	public void addFirst(Student student) {
 		// Add your implementation here
 		int len=this.students.length;
-		for(int j=len;j>0;j--)
+		for(int j=len+1;j>0;j--)
 		{
 		this.students[j]=this.students[j-1];}
 			this.students[0]=student;
-			this.students[len+1]=null;
+			this.students[len+2]=null;
 		
 	}
 
