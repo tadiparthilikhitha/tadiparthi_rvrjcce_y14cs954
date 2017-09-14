4348ff7ef4ffd0cd1252b4af901d938338387a0e
tadiparthilikhitha <tadiparthilikhitha@gmail.com>
2017-09-14 21:45:33 +0530

    My tenth commit

--- src/StudentGroup.java
+++ src/StudentGroup.java
@@ -84,19 +84,19 @@ public class StudentGroup implements StudentArrayOperation {
 	@Override
 	public void remove(int index) {
 		// Add your implementation here
-		int len=this.students.length;
+	/*	int len=this.students.length;
 		for(int j=index;j<len;j++)
 		{
 		this.students[j]=this.students[j+1];}
 
-			this.students[len+1]=null;
+			this.students[len+1]=null;*/
 	}
 
 	@Override
 	public void remove(Student student) {
 		// Add your implementation here
-		int len=this.students.length;
-		int t;
+		/*int len=this.students.length;
+		int t=0;
 		for(int j=0;j<len;j++)
 		{
 			if(this.students[j].equals(student))
@@ -108,7 +108,7 @@ public class StudentGroup implements StudentArrayOperation {
 		for(int j=t;j<len;j++)
 		this.students[j]=this.students[j+1];
 
-			this.students[len+1]=null;
+			this.students[len+1]=null;*/
 	}
 
 	@Override
